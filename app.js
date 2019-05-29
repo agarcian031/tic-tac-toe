1f309468dc7f20d655fab4cd7748c93bc6b4c09e
Amanda Garcia <algarcian@outlook.com>
2019-05-29 14:30:47 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+var items = document.getElementsByClassName("box")
+var user_choice_p = document.querySelector(".user-choice > p"); 
+
+var options = ["x", "o"]
+var userChoice = [] 
+for(var i = 0; i < items.length; i++) {
+  items[i].addEventListener("click", function() {
+    this.id = "x"
+    userChoice.push(this.id) 
+
+    // randomChoice = options[Math.floor(Math.random() * options.length)]
+    // alert(`You picked ${this.id}`)
+    user_choice_p.innerHTML =  `You picked ${this.id}`
+  })
+
+}
